1b9ea895f5c0babcb36ebb831d7b6172b5807fa1
acm87 <45886473+acm87@users.noreply.github.com>
2019-01-19 01:54:09 +0100

    actualizado excel, clase q la manipula y clase principal

--- src/binpackingproblem/BinPackingProblem.java
+++ src/binpackingproblem/BinPackingProblem.java
@@ -11,6 +11,13 @@ import dao.ManageExcel;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -31,61 +38,56 @@ public class BinPackingProblem {
             }
         });
 
-       //MPVSBPP_SET1_IT5000_ITV1_NT2_TS3_WT1_VT1_REP1.dat Se utilizan 2 tiempos porque se llena el satelite en el 1er tiempo
-       // System.out.println("Heuristic Method Value: " + ResolverController.getInstance(new File("MPVSBPP_SET1_IT5000_ITV1_NT2_TS3_WT1_VT1_REP1.dat")).heuristicResolver());
-        
-        
+        //MPVSBPP_SET1_IT5000_ITV1_NT2_TS3_WT1_VT1_REP1.dat Se utilizan 2 tiempos porque se llena el satelite en el 1er tiempo
+        // System.out.println("Heuristic Method Value: " + ResolverController.getInstance(new File("MPVSBPP_SET1_IT5000_ITV1_NT2_TS3_WT1_VT1_REP1.dat")).heuristicResolver());
         //realizamos los algoritmos para cada dataset
-       // int countOfExecutionsGRASP = 50;
-       
+        // int countOfExecutionsGRASP = 50;
         int minimumCost;
-        int sumCost=0; //heuristicResolverInicial 35427705    heuristicResolverSecond 35749895  heuristicResolverDoble 35363405
+        int sumCost = 0; //heuristicResolverInicial 35427705    heuristicResolverSecond 35749895  heuristicResolverDoble 35363405
         String excelFilePath = "results-assignment.xlsx";
-        for (int i = 0; i < files.length; i++) {
 
-            ResolverController m = ResolverController.getInstance(files[i]);
-            //ResolverController m = ResolverController.getInstance(new File("MPVSBPP_SET1_IT1000_ITV1_NT2_TS3_WT1_VT1_REP9.dat"));
-            m.c=new CreateFileResult(files[i].getName());
-             
-            m.runAndPrintSolution = 2; //0 solo run, 1 run and update the value for excel and 2 1 run, update the value for excel and print in Console
-           
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 
-            long startTime = System.currentTimeMillis();
-            minimumCost = m.heuristicResolverDoble();
-            long timeofExecution = System.currentTimeMillis() - startTime;
-            
-            sumCost+=minimumCost;
-            
-            print(m.c, files[i].getName());
-            print(m.c,"Heuristic Method Value: " + minimumCost + " Delay in milliseconds: " + timeofExecution);
-            
-            m.c.saveFile();
-            
-            
-           /* for (Map.Entry<Integer, Integer> en : m.vehiclesCountByType.entrySet()) {
-                Integer key = en.getKey(); //type of vehicle
-                Integer value = en.getValue(); //cant of vehicles
+            for (int i = 0; i < files.length; i++) {
+
+                ResolverController m = ResolverController.getInstance(files[i]);
+                //ResolverController m = ResolverController.getInstance(new File("MPVSBPP_SET1_IT1000_ITV1_NT2_TS3_WT1_VT1_REP9.dat"));
+                m.c = new CreateFileResult(files[i].getName());
+
+                m.runAndPrintSolution = 2; //0 solo run, 1 run and update the value for excel and 2 1 run, update the value for excel and print in Console
+
+                long startTime = System.currentTimeMillis();
+                minimumCost = m.heuristicResolverDoble();
+                long timeofExecution = System.currentTimeMillis() - startTime;
+
+                sumCost += minimumCost;
+
+                print(m.c, files[i].getName());
+                print(m.c, "Heuristic Method Value: " + minimumCost + " Delay in milliseconds: " + timeofExecution);
+
+                m.c.saveFile();
+
+                ManageExcel.updateExcel(workbook, files[i].getName(), minimumCost, timeofExecution, files.length, m.vehiclesCountByType, m.demandCountByTimeSlot);
+
+                //System.out.println(files[i].getName());
             }
             
-            for (Map.Entry<Integer, Integer> en : m.demandCountByTimeSlot.entrySet()) {
-                Integer key = en.getKey(); //time slot
-                Integer value = en.getValue(); //cant of demands
-            }
-            */
-            
-            //ManageExcel.updateExcel(excelFilePath, files[i].getName(), minimumCost, timeofExecution, files.length);
-            
-
-            
-            
-         //System.out.println();
+            inputStream.close();
+            FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+        } catch (Exception ex) {
+            Logger.getLogger(BinPackingProblem.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        System.out.println("sumCost "+sumCost );
+
+        System.out.println("sumCost " + sumCost);
 
     }
-    
-    public static void print(CreateFileResult c, String text){
+
+    public static void print(CreateFileResult c, String text) {
         c.setValueToPrint(text);
         //System.out.println(text);
     }
--- src/dao/ManageExcel.java
+++ src/dao/ManageExcel.java
@@ -6,15 +6,23 @@
 package dao;
 
 import controller.ResolverController;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.common.usermodel.Hyperlink;
+import org.apache.poi.common.usermodel.HyperlinkType;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Color;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFHyperlink;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
@@ -23,17 +31,30 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class ManageExcel {
 
-    public static void updateExcel(String excelFilePath, String nameFileData, int minimumCost, long timeofExecution, int filesCount) {
+    public static void updateExcel(XSSFWorkbook workbook, String nameFileData, int minimumCost, long timeofExecution, int filesCount, Map<Integer, Integer> vehiclesCountByType, Map<Integer, Integer> demandCountByTimeSlot) {
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 
         try {
+            /*
             FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
 
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-
+            */
+            
             Sheet sheet = workbook.getSheetAt(0);
 
             Iterator<Row> itRow = sheet.rowIterator();
+            
+            XSSFCellStyle hlinkstyle = workbook.createCellStyle();
+            XSSFFont hlinkfont = workbook.createFont();
+            hlinkfont.setUnderline(XSSFFont.U_SINGLE);
+            /*
+            short color = Short.valueOf("#0000ff");
+            hlinkfont.setColor(color);
+            */
+            
+            hlinkfont.setColor(IndexedColors.BLUE.getIndex());
+            hlinkstyle.setFont(hlinkfont);
 
             int i = 0;
             nameFileData = nameFileData.replace(".dat", "");
@@ -46,12 +67,12 @@ public class ManageExcel {
                     currentRow.createCell(3);
                 }
                 currentRow.getCell(3).setCellType(CellType.STRING);
-                if(currentRow.getCell(3).getStringCellValue().equals("") || currentRow.getCell(3).getStringCellValue().isEmpty()){
-                  //escribe el valor nombre del fichero
+                if (currentRow.getCell(3).getStringCellValue().equals("") || currentRow.getCell(3).getStringCellValue().isEmpty()) {
+                    //escribe el valor nombre del fichero
                     //currentRow.getCell(3).setCellType(CellType.STRING);
                     currentRow.getCell(3).setCellValue(nameFileData);
                 }
-                
+
                 // busca cual es la fila que tiene el nombre del archivo
                 if (currentRow.getCell(3).getStringCellValue().equals(nameFileData)) {
                     if (currentRow.getCell(5) == null) {
@@ -61,7 +82,25 @@ public class ManageExcel {
                     //escribe el valor del costo minimo dos celdas a la derecha
                     currentRow.getCell(5).setCellType(CellType.NUMERIC);
                     currentRow.getCell(5).setCellValue(minimumCost);
-
+                    
+                    CreationHelper createHelper = workbook.getCreationHelper();
+                    XSSFHyperlink link = (XSSFHyperlink)createHelper.createHyperlink(HyperlinkType.FILE);
+                    link.setAddress(nameFileData + ".dat");
+                    currentRow.getCell(3).setHyperlink(link);
+                    currentRow.getCell(3).setCellStyle(hlinkstyle);
+                    
+                    link = (XSSFHyperlink)createHelper.createHyperlink(HyperlinkType.FILE);
+                    link.setAddress(nameFileData + ".dat-result.txt");
+                    currentRow.getCell(5).setHyperlink(link);
+                    currentRow.getCell(5).setCellStyle(hlinkstyle);
+                    
+                    /*
+                    CreationHelper createHelper = workbook.getCreationHelper();
+                    Hyperlink link = createHelper.createHyperlink(HyperlinkType.FILE);
+                    link.setAddress(nameFileData + ".dat-result.txt");
+                    currentRow.getCell(3).setHyperlink((org.apache.poi.ss.usermodel.Hyperlink) link);
+                    */
+                    
                     if (currentRow.getCell(7) == null) {
                         currentRow.createCell(7);
                     }
@@ -78,17 +117,49 @@ public class ManageExcel {
                     currentRow.getCell(6).setCellType(CellType.FORMULA);
                     currentRow.getCell(6).setCellFormula(strFormula);
                      */
+                    //inicializando las celdas en cero
+                    int c;
+                    for (c = 11; c <= 18; c++) {
+                        if (c != 15) {
+                            if (currentRow.getCell(c) == null) {
+                                currentRow.createCell(c);
+                            }
+                            currentRow.createCell(c);
+                            currentRow.getCell(c).setCellType(CellType.NUMERIC);
+                            currentRow.getCell(c).setCellValue(0);
+                        }
+                    }
+                    
+                    //inicializamos c en 10 pq es el numero de celda anterior a los tipos de vehiculos
+                    c = 10;
+                    for (Map.Entry<Integer, Integer> en : vehiclesCountByType.entrySet()) {
+                        Integer key = en.getKey(); //type of vehicle
+                        Integer value = en.getValue(); //cant of vehicles
+                        currentRow.getCell(c + key).setCellValue(value);                        
+                    }
+                    
+                    //inicializamos c en 10 pq es el numero de celda anterior a los intervalos de tiempo
+                    c = 16;
+                    for (Map.Entry<Integer, Integer> en : demandCountByTimeSlot.entrySet()) {
+                        Integer key = en.getKey(); //time slot
+                        Integer value = en.getValue(); //cant of demands
+                        currentRow.getCell(c + key).setCellValue(value); 
+                    }
+
                     i = filesCount + 1;
                 }
 
+                //System.out.println("ManageExcel" + i);
                 i++;
             }
+            
+            /*
             inputStream.close();
-
             FileOutputStream outputStream = new FileOutputStream(excelFilePath);
             workbook.write(outputStream);
             workbook.close();
             outputStream.close();
+            */
         } catch (Exception ex) {
             Logger.getLogger(ResolverController.class.getName()).log(Level.SEVERE, null, ex);
         }
